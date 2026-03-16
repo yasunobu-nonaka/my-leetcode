51404efb7d51bbd6ac009ad782a3af2913a118dd
yasunobu-nonaka <74456551+yasunobu-nonaka@users.noreply.github.com>
2026-03-16 23:42:35 +0900

    improve my answer for stack Q3 to pass (passed)
    - to make process faster for the case ["0:start:0", "0:end:100000000"], abolish step by step time processing
    - Before: step time one by one. After: step each log in logs

--- stack/q3.py
+++ stack/q3.py
@@ -1,52 +1,57 @@
 class Solution:
     def exclusiveTime(self, n: int, logs: List[str]) -> List[int]:
+        pointer = 0
+        current_time = 0
         stack = []
         exclusive_time = [0] * n
-        time = 0
-        is_time_proceed = True
-        end_time = int(logs[-1].split(":")[2])
 
-        while time <= end_time:
-            head = logs[0]
-            head_id = head.split(":")[0]
-            head_event = head.split(":")[1]
-            head_time = int(head.split(":")[2])
+        # logsを一つずつ処理していく。
+        while pointer < len(logs) - 1:
+            current_arr = logs[pointer].split(":")
+            next_arr = logs[pointer + 1].split(":")
 
-            if time == head_time and head_event == "start":
-                stack.append(int(head_id))
-                if is_time_proceed:
-                    exclusive_time[stack[-1]] += 1
-                logs.pop(0)
-            elif time == head_time and head_event == "end":
-                if is_time_proceed:
-                    exclusive_time[stack[-1]] += 1
+            current_time = int(current_arr[2])
+            current_event = current_arr[1]
+
+            # 今のlogがstartであればstackにPush、endであればPopする
+            # 追加する値は関数のID
+            if current_event == "start":
+                stack.append(int(current_arr[0]))
+            else:
                 stack.pop()
-                logs.pop(0)
-            else:
-                if is_time_proceed:
-                    exclusive_time[stack[-1]] += 1
 
-            if len(logs) != 0:
-                next_time = int(logs[0].split(":")[2])
-                if next_time > time:
-                    is_time_proceed = True
-                    time += 1
-                else:
-                    is_time_proceed = False
+            next_time = int(next_arr[2])
+            time_proceeds = next_time - current_time
+
+            next_event = next_arr[1]
+            # 今と次のイベントによってexclusive timeを変化させる値を調整する。
+            # start -> endの場合時間差に１を足す
+            if current_event == "start" and next_event == "end":
+                # stackのトップのIDをインデックスとしてexclusive_timeに値を足す
+                exclusive_time[stack[-1]] += time_proceeds + 1
+            # end -> startの場合時間差から１を引く
+            elif current_event == "end" and next_event == "start":
+                # stackの要素が1つのときにendになるとstackが空になってしまうので確認
+                if len(stack) != 0:
+                    exclusive_time[stack[-1]] += time_proceeds - 1
             else:
-                is_time_proceed = True
-                time += 1
+                exclusive_time[stack[-1]] += time_proceeds
+
+            pointer += 1
 
         return exclusive_time
 
 
 if __name__ == "__main__":
-    n = 2
+    n = 3
+    # n = 2
     # n = 1
-    logs = ["0:start:0", "1:start:2", "1:end:5", "0:end:6"]
+    # logs = ["0:start:0", "1:start:2", "1:end:5", "0:end:6"]
     # logs = ["0:start:0", "0:start:2", "0:end:5", "0:start:6", "0:end:6", "0:end:7"]
     # logs = ["0:start:0", "0:start:2", "0:end:5", "1:start:6", "1:end:6", "0:end:7"]
     # logs = ["0:start:0", "0:end:100000000"]
+    # logs = ["0:start:0", "0:start:2", "0:end:5", "1:start:7", "1:end:7", "0:end:8"]
+    logs = ["0:start:0", "0:end:0", "1:start:1", "1:end:1", "2:start:2", "2:end:2", "2:start:3", "2:end:3"]
     print(logs, "\n")
     solution = Solution()
     result = solution.exclusiveTime(n, logs)
