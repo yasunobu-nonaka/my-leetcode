e48393b40d66068077e7b765b6bb612b2682ef2e
yasunobu-nonaka <74456551+yasunobu-nonaka@users.noreply.github.com>
2026-03-18 21:48:37 +0900

    small improved answer for Filtering & Aggregation Q4 suggested by chatgpt. (LeetCode, Quest, Database, Filtering & Aggregation, Q4. User Activity for the Past 30 Days I)

--- sql-filtering-aggregation/q4.sql
+++ sql-filtering-aggregation/q4.sql
@@ -3,7 +3,7 @@ SELECT
     activity_date AS day,
     COUNT(DISTINCT user_id) AS active_users
 FROM Activity
-WHERE activity_date >= DATE '2019-07-27' - 29 and activity_date <= DATE '2019-07-27'
+WHERE activity_date >= DATE '2019-07-27' - 29
+    AND activity_date <= DATE '2019-07-27'
 GROUP BY activity_date
-HAVING COUNT(DISTINCT user_id) >= 1
 ORDER BY activity_date;
